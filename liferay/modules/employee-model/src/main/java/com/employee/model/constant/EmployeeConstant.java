f66c45ccaf1e171aeb668ec0c3f6676ec0c112af
maitri <maitripatel9963@gmail.com>
2025-07-22 15:56:18 +0530

    Added Constant file

---
+++ liferay/modules/employee-model/src/main/java/com/employee/model/constant/EmployeeConstant.java
@@ -0,0 +1,10 @@
+package com.employee.model.constant;
+
+public class EmployeeConstant {
+	
+	public static final String Delete = "delete";
+	
+	public static final String INSERT = "insert";
+	
+	public static final String UPDATE = "update";
+}
