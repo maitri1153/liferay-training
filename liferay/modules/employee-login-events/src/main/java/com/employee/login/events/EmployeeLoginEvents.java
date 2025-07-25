fd376177ba265be49aa184c5b548ea3434eea1fc
maitri <maitripatel9963@gmail.com>
2025-07-25 12:30:29 +0530

    Remove PortalUtil and added portal reference

--- liferay/modules/employee-login-events/src/main/java/com/employee/login/events/EmployeeLoginEvents.java
+++ liferay/modules/employee-login-events/src/main/java/com/employee/login/events/EmployeeLoginEvents.java
@@ -19,7 +19,7 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 
 @Component(
@@ -37,11 +37,11 @@ public class EmployeeLoginEvents implements LifecycleAction {
 		try {
 			log.info("Post Login event is Started");
 			
-			HttpServletRequest request = PortalUtil.getOriginalServletRequest(lifecycleEvent.getRequest());
+			HttpServletRequest request = portal.getOriginalServletRequest(lifecycleEvent.getRequest());
 			String ipAddress = request.getRemoteAddr();
 			log.info("Client IP Address: " + ipAddress);
 			
-			long companyId = PortalUtil.getUser(request).getCompanyId();
+			long companyId = portal.getUser(request).getCompanyId();
 			
 			Group group = groupLocalService.getGroup(companyId, EmployeeLoginConstant.IGNEK_INTRANET);
 			long groupId = group.getGroupId();
@@ -54,12 +54,12 @@ public class EmployeeLoginEvents implements LifecycleAction {
 				ServiceContext serviceContext = new ServiceContext();
 				Map<String, Serializable> values = new HashMap<>();
 				values.put(EmployeeLoginConstant.ACTIVITY_TYPE,EmployeeLoginConstant.LOGIN);
-				values.put(EmployeeLoginConstant.DETAILS, PortalUtil.getUser(request).getEmailAddress());
+				values.put(EmployeeLoginConstant.DETAILS, portal.getUser(request).getEmailAddress());
 				values.put(EmployeeLoginConstant.IP_ADDRESS, ipAddress);
 				log.info("Values are set for activity");
 				
 				ObjectEntry objectEntry = objectEntryLocalService
-					.addObjectEntry(PortalUtil.getUser(request).getUserId() ,groupId,
+					.addObjectEntry(portal.getUser(request).getUserId() ,groupId,
 						objectDefinition.getObjectDefinitionId(), values ,serviceContext);
 				
 				log.info("Object entry created with ID: " + objectEntry.getObjectEntryId());
@@ -79,4 +79,7 @@ public class EmployeeLoginEvents implements LifecycleAction {
 	
 	@Reference 
 	GroupLocalService groupLocalService;
+	
+	@Reference
+	Portal portal;
 }
